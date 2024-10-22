b63ecbc74cf9f5dba70b2ef66f1b717314f10c59
Surajit Bose <verbose@duck.com>
2024-10-22 14:00:05 -0700

    Add week 5 slides and template

---
+++ week_05/template.py
@@ -0,0 +1,36 @@
+"""
+filename.py
+
+Description of problem
+
+Programmer: Your name here, Date: date modified here
+"""
+
+# import statements
+
+# CONSTANTS
+
+def main():
+    """
+    Brief overview of what main() does
+    
+    Preconditions:
+    
+    Postconditions:
+    """
+    # write out high-level steps here
+
+
+
+# Helper functions, as many as needed to implement the high-level steps
+def helper…func(): # use a short but descriptive name!
+    """
+    Brief description of what this function does
+    
+    Preconditions:
+    
+    Postconditions:
+    """
+
+if __name__ == "__main__":
+    main()
