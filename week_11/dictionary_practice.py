faabb7ec4189d2b525fb338e46a14f44a9971eaa
Surajit Bose <verbose@duck.com>
2024-12-04 15:33:27 -0800

    Add week 11 slides and sample code

---
+++ week_11/dictionary_practice.py
@@ -0,0 +1,33 @@
+def main():
+    # Create a new dictionary with first names as keys
+    # and favorite cuisines as values. E.g., if Linh's
+    # favorite cuisine is Thai, then the key : value pair
+    # should be 'Linh' : 'Thai' 
+    
+    fave_cuisine = # create and fill the dictionary here
+
+    # Okay, now let's add Mehran to the dictionary. 
+    # His favorite cuisine is Chinese.
+    
+    fave_cuisine # what goes here?
+
+    # Oops! I was wrong. Mehran corrected me: his
+    # favorite cuisine is Ethiopian.
+    
+    fave_cuisine # what goes here?
+
+    # Check whether Lane's favorite cuisine is part of the dictionary.
+    # If it is, print "Lane's favorite food is {cuisine}"
+    # If it is not, print "Lane's culinary preferences are unknown"
+    if # what goes here?
+
+    # print out all the key-value pairs in the dictionary
+    # in the format: 
+    # {name}'s favorite food is {cuisine}
+    # e.g.,
+    # Mehran's favorite food is Ethiopian
+    
+    # replace this line with code to print out all the pairs
+
+if __name__ == "__main__":
+    main()
